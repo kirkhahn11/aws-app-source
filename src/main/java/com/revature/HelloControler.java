96dbaab1f35d7c351b05275a9e1f06c6873fdfcb
Kirk Hahn <kirkhahn11@yahoo.com>
2021-09-17 10:10:23 -0500

    another push

--- src/main/java/com/revature/HelloControler.java
+++ src/main/java/com/revature/HelloControler.java
@@ -4,12 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/hello")
+@RequestMapping("/hello") //can be reached at localhost:8080/hello
 @RestController
 public class HelloControler {
 	
 	@GetMapping
 	public String hello() { 
+		
+		//just writing a comment
 		return "What's good?!";
 	}
 }
